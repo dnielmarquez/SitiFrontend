b91ed8589a2e1c9d6d50c27965f7c1cc36fdc01f
Daniel Marquez <marquezlink@hotmail.com>
2024-04-15 18:20:15 -0500

    new build

--- dist/assets/otp-e9f9c9c3.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as a,T as r}from"./index-59e4e7c3.js";const e=()=>(s(),a.jsxs(a.Fragment,{children:[a.jsx(r,{variant:"h5",sx:{mb:2},children:"User Validation"}),a.jsx(r,{sx:{mb:4},children:"Please wait for the administrator to approve your user."})]}));export{e as default};
---
+++ dist/assets/seo-07d37a58.js
@@ -0,0 +1,1 @@
+import{aa as r,j as s,ab as o}from"./index-06516407.js";const a=e=>{const{title:t}=e,i=t?t+" | Devias Kit PRO":"Devias Kit PRO";return s.jsx(o,{children:s.jsx("title",{children:i})})};a.propTypes={title:r.string};export{a as S};
