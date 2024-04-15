009422e50ab7bbca8f7dfe8f6f7d1534b7c6bdba
Daniel Marquez <marquezlink@hotmail.com>
2024-04-15 18:17:44 -0500

    good api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3005/api',
+    baseURL: 'http://3.145.178.68/api',
     timeout: 100000,
     headers: {
         'Content-Type': 'application/json',
